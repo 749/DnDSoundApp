02416fe97e2cbc7f572a3591bd604836b7e9a3cc
Jan Giesenberg <jan@giesenberg.de>
2017-07-15 08:54:31 +0200

    Finished Scrubber component

--- src/assets/vue/components/sbScrubber/sbScrubber.vue
+++ src/assets/vue/components/sbScrubber/sbScrubber.vue
@@ -1,5 +1,7 @@
 <template>
-	<md-progress class="sb-scrubber" :md-progress="pos"></md-progress>
+	<div @mousedown="startScrub">
+	<md-progress class="sb-scrubber" :md-progress="progress"></md-progress>
+	</div>
 </template>
 
 <script>
@@ -9,10 +11,36 @@ export default {
 	},
 	data() {
 		return {
+			mMove: null,
+			mUp: null,
+			scrubPos: null
+		}
+	},
+	computed: {
+		progress() {
+			return (this.scrubPos == null)? this.pos: this.scrubPos
 		}
 	},
 	methods: {
-		
+		startScrub(evt) {
+			this.mUp = this.endScrub.bind(this)
+			document.body.addEventListener("mouseup", this.mUp)
+			this.mMove = this.scrub.bind(this)
+			document.body.addEventListener("mousemove", this.mMove)
+			
+			this.scrub(evt)
+		},
+		scrub(evt) {
+			this.scrubPos = (evt.clientX * 100) / document.body.clientWidth;
+		},
+		endScrub(evt) {
+			document.body.removeEventListener("mousemove", this.mMove);
+			document.body.removeEventListener("mouseup", this.mUp);
+			
+			this.$emit('scrub', this.scrubPos)
+			
+			this.scrubPos = null
+		}
 	}
 }
 </script>
